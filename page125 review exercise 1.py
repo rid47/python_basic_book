18f7585dfdf32e9d65b682dca4cdf5673a84d065
rid47 <ridwanmizan@gmail.com>
2020-03-14 07:18:42 +0600

    review exercise page 125

---
+++ page125 review exercise 1.py
@@ -0,0 +1,2 @@
+a = float(input("Enter a number: "))
+print(round(a, 2))
---
+++ page125 review exercise 3.py
@@ -0,0 +1,4 @@
+a = float(input("Enter a number: "))
+b = float(input("Enter another number: "))
+c = a-b
+print(f"The difference between 1.5 and 1.0 is an integer? {c.is_integer()}")
